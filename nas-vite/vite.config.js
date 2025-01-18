ecb6e8d45aaf567def1210787dad9cc497f6ede1
Joe Morrissey <GameDesignerJoe@gmail.com>
2025-01-17 21:54:29 -0800

    FIX: Update start_url and scope in manifest.json for correct routing

--- nas-vite/vite.config.js
+++ nas-vite/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nas-vite/', // Replace 'nas-vite' with your repository name
+  base: '/nas-vite/',  // This matches your repository name
 })
