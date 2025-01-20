78ef43cfebdab96078ba6f88073d32150bb9559b
Joe Morrissey <GameDesignerJoe@gmail.com>
2025-01-20 02:31:17 -0800

    FEAT: Add PWA support with manifest file, service worker, and mobile meta tags

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
+    event.waitUntil(
+      caches.open('static-cache').then((cache) => 
+        cache.addAll(['./', './index.html', './manifest.json'])
+      )
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => 
+        response || fetch(event.request)
+      )
+    );
+  });
+  
