9039525f6775d601ce73847524faa6e36bde573a
Joe Morrissey <GameDesignerJoe@gmail.com>
2025-02-09 01:12:40 -0800

    FEAT: Update HTML and manifest for improved mobile support and visual consistency

--- nas-vite/vite.config.js
+++ nas-vite/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/GameDesignerJoe.github.io/',
   publicDir: resolve(__dirname, 'public'),
   server: {
     port: 5173,
