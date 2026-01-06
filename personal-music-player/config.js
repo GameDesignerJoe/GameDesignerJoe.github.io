8f0ed4e2bb942962e08b966fa51dd7cf6b35bfca
Joe Morrissey <GameDesignerJoe@gmail.com>
2026-01-06 12:31:26 -0800

    Fix redirect URI trailing slash

--- personal-music-player/config.js
+++ personal-music-player/config.js
@@ -3,7 +3,7 @@ const config = {
   dropboxAppKey: 'w6g3az21d8acw15',
   redirectUri: window.location.hostname === 'localhost' 
     ? 'http://localhost:8080/'
-    : window.location.origin,
+    : window.location.origin + '/',
   
   // Supported audio file extensions
   audioExtensions: ['.mp3', '.m4a', '.flac', '.wav', '.ogg', '.opus', '.webm', '.aac'],
