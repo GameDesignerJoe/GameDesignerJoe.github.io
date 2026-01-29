699adfe20c68f066890ae1868e036a04908c68ae
GameDesignerJoe <GameDesignerJoe@gmail.com>
2026-01-29 14:12:02 -0800

    Delete ship-life directory

--- ship-life/editor/client/start-client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting FellowDivers JSON Editor Client...
-echo.
-echo Make sure the server is running first!
-echo (Double-click start-server.bat in the editor folder)
-echo.
-cd /d "%~dp0"
-npm run dev
-pause
